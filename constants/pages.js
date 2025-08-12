94a844b00bc6f1b29275a4b5e06891049a7c9c0f
caulfield27 <al1onggwp@gmail.com>
2025-08-12 11:18:56 +0500

    handle SEO optimization

--- constants/pages.js
+++ constants/pages.js
@@ -2,19 +2,19 @@ export const pages = [
     {
         name: "Сапёр",
         icon: "./assets/minesweeper_preview.avif",
-        href: "./pages/minesweeper/minesweeper.html",
+        href: "minesweeper.html",
         tags: ["2D","Puzzle","Singleplayer"]
     },
     {
         name: "Найди пару",
         icon: "./assets/memory.avif",
-        href: "./pages/memoryGame/memory-game.html",
+        href: "memory-game.html",
         tags: ["2D","Memory","Singleplayer"]
     },
     {
         name: "Морской бой",
         icon: "./assets/battleship_preview.avif",
-        href: "./pages/battleship/battleship.html",
+        href: "battleship.html",
         tags: ["2D","Strategy","Multiplayer","Board Game"]
     }
 ]
